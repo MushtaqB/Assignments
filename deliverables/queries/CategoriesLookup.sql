a7db7e9a774710927cff57fb1547a1f1081cb02d
MushtaqB <urfriendmushi@gmail.com>
2015-08-17 19:24:24 +0300

    Missed deliverables
    Missed deliverables

---
+++ deliverables/queries/CategoriesLookup.sql
@@ -0,0 +1,14 @@
+
+INSERT INTO `assignment`.`tbl_productmgmt_categories`
+(`AISLE_ID`,`DESCRIPTION`,`NAME`,`DEPARTMENT_ID`)
+VALUES
+(1,'Pans','Pans',1),
+(1,'Knives','Knives',1),
+(2,'Vegetables','Vegetables',2),
+(2,'fruits','fruits',2),
+(3,'Luggage','Luggage',3),
+(3,'Gifts','Gifts',3),
+(4,'TV','TV',4),
+(4,'Washing Machine','Washing Machine',4),
+(5,'BOOKS','BOOKS',5),
+(5,'PENS','PENS',5);
---
+++ deliverables/queries/DepartmentsLookup.sql
@@ -0,0 +1,8 @@
+INSERT INTO `assignment`.`tbl_productmgmt_departments`
+ (`DESCRIPTION`,`LOCATION_ID`,`NAME`)
+ VALUES
+ ('KitchenWare',1,'KitchenWare'),
+ ('Groceries',2,'Groceries'),
+ ('Travel',3,'Travel'),
+ ('Electronics',4,'Electronics'),
+ ('Stationary',5,'Stationary');
